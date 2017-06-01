dff9e59dabed3a32e3b670a0a1c1839ca050d031
Hamish Taplin <hello@hamishtaplin.com>
2017-06-01 13:09:37 +0100

    implemented CORS

--- src/schema.js
+++ src/schema.js
@@ -5,16 +5,16 @@ var {
   GraphQLSchema,
   GraphQLList,
   GraphQLNonNull
-} = require('graphql');
+} = require("graphql");
 
 // Import the Database connection.
-var Db = require('./db')
+var Db = require("./db");
 
 // Define the GraphQL types for the schema.
 // Eventually these might include things like Pages, Questions, Answers etc.
 var Message = new GraphQLObjectType({
-  name: 'Message',
-  description: 'This represents a message',
+  name: "Message",
+  description: "This represents a message",
   fields: () => {
     return {
       id: {
@@ -29,32 +29,32 @@ var Message = new GraphQLObjectType({
           return message.text;
         }
       }
-    }
+    };
   }
 });
 
 // Define a root query.
 // Think of this as public API for querying
 var Query = new GraphQLObjectType({
-  name: 'Query',
-  description: 'This is the root query',
+  name: "App",
+  description: "This is the root query",
   fields: () => {
     return {
       hello: {
         type: new GraphQLList(Message),
         resolve(root, args) {
-          return Db.models.message.findAll({where: args});
+          return Db.models.message.findAll({ where: args });
         }
       }
-    }
+    };
   }
 });
 
 // Define a root mutation.
 // Think of this as public API for mutation
 var Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Functions to mutate stuff',
+  name: "Mutation",
+  description: "Functions to mutate stuff",
   fields() {
     return {
       // In this case I'm definining a single mutation object that adds a new
@@ -73,7 +73,7 @@ var Mutation = new GraphQLObjectType({
           });
         }
       }
-    }
+    };
   }
 });
 
@@ -87,4 +87,4 @@ var schema = new GraphQLSchema({
 });
 
 // Export the schema
-module.exports = schema
+module.exports = schema;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var schema = require("./schema");
+var colors = require("colors");
+var { url, port } = require("./settings").express;
+var { endpoint, enabled, pretty } = require("./settings").graphiql;
 
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var schema = require('./schema');
-var colors = require('colors');
-var { url, port } = require('./settings').express
-var { endpoint, enabled, pretty } = require('./settings').graphiql
-
+const cors = require("cors");
 var app = express();
-app.use(endpoint, graphqlHTTP({
-  schema: schema,
-  pretty: pretty,
-  graphiql: enabled,
-}));
 
-console.log('Starting server...')
+app.use(
+  endpoint,
+  cors(),
+  graphqlHTTP({
+    schema: schema,
+    pretty: pretty,
+    graphiql: enabled
+  })
+);
+
+console.log("Starting server...");
 app.listen(port, () => {
-  console.log(colors.green('eq-author-api'),
-    'is running at',
-    colors.yellow(url + endpoint))
+  console.log(
+    colors.green("eq-author-api"),
+    "is running at",
+    colors.yellow(url + endpoint)
+  );
 });
