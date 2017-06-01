111c8f660e2b2a82c5ce9995c7e1dafc85bc75eb
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-06-01 15:12:56 +0100

    Remove duplication that was introduced by a merge.

--- src/server.js
+++ src/server.js
@@ -5,16 +5,9 @@ var colors = require("colors");
 const cors = require("cors");
 var { EXPRESS_URL, EXPRESS_PORT } = require('./settings').express
 var {
-   GRAPHIQL_ENABLED,
-   GRAPHIQL_ENDPOINT,
-   GRAPHIQL_PRETTY
- } = require('./settings').graphiql
-
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var schema = require('./schema');
-var colors = require('colors');
-
+  GRAPHIQL_ENABLED,
+  GRAPHIQL_ENDPOINT,
+  GRAPHIQL_PRETTY } = require('./settings').graphiql
 
 var app = express();
 app.use(GRAPHIQL_ENDPOINT,
