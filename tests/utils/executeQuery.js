b1f7ceab858429882c3f2c34a4876af2736fcc4f
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-07-05 11:27:50 +0100

    Remove async from the function.

--- tests/utils/executeQuery.js
+++ tests/utils/executeQuery.js
@@ -1,7 +1,7 @@
 const schema = require("../../schema");
 const { graphql } = require("graphql");
 
-async function executeQuery(query, args = {}, ctx = {}) {
+function executeQuery(query, args = {}, ctx = {}) {
     return graphql(schema, query, {}, ctx, args);
 }
 
