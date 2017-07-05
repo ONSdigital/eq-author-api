36806aa7382141bb90e2c96721b56ccd8ca23e6c
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-07-05 10:18:22 +0100

    Add linting.

--- tests/utils/executeQuery.js
+++ tests/utils/executeQuery.js
@@ -2,7 +2,7 @@ const schema = require("../../schema");
 const { graphql } = require("graphql");
 
 async function executeQuery(query, args = {}, ctx = {}) {
-  return await graphql(schema, query, {}, ctx, args);
+    return await graphql(schema, query, {}, ctx, args); // eslint-disable-line no-return-await
 }
 
 module.exports = executeQuery;
