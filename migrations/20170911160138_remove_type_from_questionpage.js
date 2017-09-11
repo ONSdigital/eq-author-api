6b68f9c3d4adee5093320b700f17c24e70d4438a
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-11 16:09:05 +0100

    Fix linting error in the migration file.

--- migrations/20170911160138_remove_type_from_questionpage.js
+++ migrations/20170911160138_remove_type_from_questionpage.js
@@ -1,10 +1,10 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.table("Pages", function(table) {
     table.dropColumn("type");
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.table("Pages", function(table) {
     table
       .string("type")
