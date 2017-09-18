ee4b5713d28a4bc0e30034c4afa32854f40dc453
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-18 16:29:00 +0100

    Fix linting errors.

--- repositories/QuestionnaireRepository.js
+++ repositories/QuestionnaireRepository.js
@@ -1,10 +1,9 @@
-const { head, map, invert } = require("lodash/fp");
+const { head, map } = require("lodash/fp");
 const Questionnaire = require("../db/Questionnaire");
 const mapFields = require("../utils/mapFields");
 
 const mapping = { created_at: "createdAt" };
 const fromDb = mapFields(mapping);
-const toDb = mapFields(invert(mapping));
 
 module.exports.get = function(id) {
   return Questionnaire.findById(id).then(fromDb);
