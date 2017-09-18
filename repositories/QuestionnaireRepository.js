e7cdfb5227526368fe3d102a9a00deaaec49dc8e
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-18 15:46:47 +0100

    Amend API to bring back createdAt date.

--- repositories/QuestionnaireRepository.js
+++ repositories/QuestionnaireRepository.js
@@ -1,43 +1,62 @@
-const { head } = require("lodash");
+const { head, map, invert } = require("lodash/fp");
 const Questionnaire = require("../db/Questionnaire");
+const mapFields = require("../utils/mapFields");
 
+const mapping = { created_at: "createdAt" };
+const fromDb = mapFields(mapping);
+const toDb = mapFields(invert(mapping));
 
 module.exports.get = function(id) {
-  return Questionnaire.findById(id);
+  return Questionnaire.findById(id).then(fromDb);
 };
 
-module.exports.findAll = function findAll(where, orderBy = "created_at", direction = "asc") {
-  return Questionnaire.findAll(where).orderBy(orderBy, direction);
+module.exports.findAll = function findAll(
+  where,
+  orderBy = "created_at",
+  direction = "asc"
+) {
+  return Questionnaire.findAll(where)
+    .orderBy(orderBy, direction)
+    .then(map(fromDb));
 };
 
-module.exports.insert = function({ title, description, theme, legalBasis, navigation, surveyId }) {
-  return Questionnaire
-    .create({
-      title,
-      description,
-      theme,
-      legalBasis,
-      navigation,
-      surveyId
-    })
-    .then(head);
+module.exports.insert = function({
+  title,
+  description,
+  theme,
+  legalBasis,
+  navigation,
+  surveyId
+}) {
+  return Questionnaire.create({
+    title,
+    description,
+    theme,
+    legalBasis,
+    navigation,
+    surveyId
+  }).then(head);
 };
 
 module.exports.remove = function(id) {
-  return Questionnaire
-    .destroy(id)
-    .then(head);
+  return Questionnaire.destroy(id).then(head);
 };
 
-module.exports.update = function({ id, title, description, theme, legalBasis, navigation, surveyId }) {
-  return Questionnaire
-    .update(id, {
-      title,
-      surveyId,
-      description,
-      theme,
-      legalBasis,
-      navigation
-    })
-    .then(head);
-}
+module.exports.update = function({
+  id,
+  title,
+  description,
+  theme,
+  legalBasis,
+  navigation,
+  surveyId
+}) {
+  return Questionnaire.update(id, {
+    title,
+    surveyId,
+    description,
+    theme,
+    legalBasis,
+    navigation
+  }).then(head);
+};
