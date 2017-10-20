6a58aa5116f2b721ff53a493df700add4d8c81d0
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-10-20 14:09:31 +0100

    Add structured logging of req/res and logging of GraphQL errors.

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
+/* eslint-disable no-console */
 const express = require("express");
 const { graphqlExpress, graphiqlExpress } = require("graphql-server-express");
+const { addErrorLoggingToSchema } = require("graphql-tools");
 const repositories = require("./repositories");
 const colors = require("colors");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const schema = require("./schema");
-const morgan = require("morgan");
-
+const pino = require("express-pino-logger")();
 const { PORT } = require("./config/settings");
 
 const app = express();
-app.use(morgan("tiny"));
+
+app.use(pino);
+
+const logger = {
+  log: error => {
+    console.error(error);
+    return error;
+  }
+};
+
+const withErrorLogging = schema => {
+  addErrorLoggingToSchema(schema, logger);
+  return schema;
+};
 
 app.use(
   "/graphql",
   cors(),
   bodyParser.json(),
-  graphqlExpress({ schema: schema, context: { repositories: repositories } })
+  graphqlExpress({
+    schema: withErrorLogging(schema),
+    context: { repositories },
+    formatError: logger.log
+  })
 );
 
 if (process.env.NODE_ENV === "development") {
@@ -24,5 +42,5 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(colors.green("Listening on port"), PORT); // eslint-disable-line
+  console.log(colors.green("Listening on port"), PORT);
 });
