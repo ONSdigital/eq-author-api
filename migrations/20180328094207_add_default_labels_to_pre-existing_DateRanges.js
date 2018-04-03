55499dae7bdf531bd5454ac713e7c99c6c5a22bd
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2018-04-03 10:22:24 +0100

    Fixed formatting issues

--- migrations/20180328094207_add_default_labels_to_pre-existing_DateRanges.js
+++ migrations/20180328094207_add_default_labels_to_pre-existing_DateRanges.js
@@ -1,10 +1,10 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex("Answers")
     .where({ type: "DateRange" })
     .update({ label: "Period from", secondaryLabel: "Period to" });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex("Answers")
     .where({ type: "DateRange" })
     .update({ label: null, secondaryLabel: null });
--- tests/schema/queries/answers.test.js
+++ tests/schema/queries/answers.test.js
@@ -8,7 +8,7 @@ describe("answers query", () => {
         label
         ... on BasicAnswer {
           secondaryLabel
-          }
+        }
         id
       }
     }
